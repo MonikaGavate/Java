88aeb0897076feb7363085d9cf9599961bb5a48f
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-01-26 21:56:37 +0530

    Add files via upload

---
+++ practical/hierarchicalEasy.java
@@ -0,0 +1,41 @@
+package monika;
+class Addition{
+	public void add()
+	{
+		int x,y=30,z=10;
+		x=y+z;
+		System.out.println("Add="+x);
+	}
+}
+//extending Addition
+class Subtraction extends Addition{
+	void sub()
+	{
+		int x,y=30,z=10;
+		x=y-z;
+		System.out.println("Sub="+x);
+	}
+}
+//extending same class Addition
+class Multiplication extends Addition{
+	void Multi()
+	{
+		int x,y=30,z=10;
+		x=y*z;
+		System.out.println("Multiply="+x);
+	}
+}
+public class hierarchicalEasy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Creating instance(object)
+		Multiplication obj=new Multiplication();
+		//calling base class function
+		obj.add();
+		//calling derive class function
+		obj.Multi();
+
+	}
+
+}
---
+++ practical/multilevelinheritance.java
@@ -0,0 +1,31 @@
+package monika;
+ 
+class Animal {
+	void eat()
+	{
+		System.out.println("eating...");
+	}
+}
+class Dog extends Animal{
+	void bark()
+	{
+		System.out.println("barking...");
+	}
+}
+class BabyDog extends Dog{
+	void weep()
+	{
+		System.out.println("weeping...");
+	}
+}
+public class multilevelinheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     BabyDog b=new BabyDog();
+      b.weep();
+      b.bark();
+      b.eat();
+	}
+
+}
