9c85b3e0fe112fc9b2b646e628def049796ebfd5
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-01-26 21:54:47 +0530

    Update and rename inheritance to singleinheritance.java

---
+++ practical/singleinheritance.java
@@ -0,0 +1,15 @@
+package monika;
+
+class Employee{
+	float salary=20000;
+}
+class singleinheritance extends Employee{
+	int bonus=5000;
+	
+	public static void main(String[] args) {
+		singleinheritance si=new singleinheritance();
+	System.out.println("Salary of developer is:"+si.salary);
+	System.out.println("Bonus of developer is:"+si.bonus);	
+	}
+}
+
