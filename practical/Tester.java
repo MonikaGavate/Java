bc2a01778e1e7d9f2fdd4517cc3d8230fa211b9d
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-01-27 16:11:28 +0530

    Add files via upload

---
+++ practical/Tester.java
@@ -0,0 +1,29 @@
+package monika;
+class shape{
+	void display()
+	{
+		System.out.println("inside display");
+	}
+}
+class rectangle extends shape{
+	void area()
+	{
+		System.out.println("inside area");
+	}
+}
+class cube extends rectangle{
+	void volume()
+	{
+		System.out.println("inside volume");
+	}
+}
+public class Tester {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       cube c=new cube();
+       c.volume();
+       c.area();
+       c.display();
+	}
+
+}
