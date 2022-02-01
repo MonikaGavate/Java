ec709aff46e4cd48fb2bb4166a409ac1ae240309
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-01 15:01:43 +0530

    Add files via upload

---
+++ practical/ImmutableDemo.java
@@ -0,0 +1,13 @@
+package monika;
+
+public class ImmutableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         String a="Monika";
+         a.concat("Gavate");
+         a=a.concat("Gavate");
+         System.out.println(a);
+	}
+
+}
