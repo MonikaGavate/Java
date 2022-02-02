c243787a00ae949e2b227c362a2a897698ddfc00
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-02 16:24:32 +0530

    Add files via upload

---
+++ practical/ArrayEx3.java
@@ -0,0 +1,12 @@
+package monika;
+
+public class ArrayEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a[]= {1,2,3,4,5};
+		 for(int i:a)
+			 System.out.println(i);
+	}
+
+}
