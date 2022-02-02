00ad81dc7fe2d23d4eda29ca9b7766f94976e574
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-02 16:22:25 +0530

    Add files via upload

---
+++ practical/ArrayEx2.java
@@ -0,0 +1,12 @@
+package monika;
+
+public class ArrayEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a[]= {1,2,3,4,5};//initializing and instantiating
+       for(int i=0;i<a.length;i++)//length is the property of array
+	   System.out.println(a[i]);
+	}
+
+}
