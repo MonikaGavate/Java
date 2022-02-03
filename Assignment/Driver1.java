c08a30389f4e5303bf544e766be003e7cb36ec1e
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-03 17:09:48 +0530

    Add files via upload

---
+++ Assignment/Driver1.java
@@ -0,0 +1,13 @@
+package monika;
+
+public class Driver1{ 
+public static void main ( String [] args ) 
+{
+ String [] strArr = new String [10]; 
+for (int i = strArr . length - 1; i >= 0; i--) 
+{
+ strArr [ i ] = "b" + ( i - 1) ; 
+}
+ System . out . println (" Value : " + strArr [5]) ;
+ }
+ }
