9e6a59583a52985f08c95e20877541d1c555a827
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-03 14:57:07 +0530

    Add files via upload

---
+++ practical/MultidimensionalLength.java
@@ -0,0 +1,13 @@
+package monika;
+
+public class MultidimensionalLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] a= { {1,2,3},{2,3,4}, {3,4,5} };
+	     System.out.println("length of row l:"+a[0].length);
+	     System.out.println("length of row l:"+a[1].length);
+	     System.out.println("length of row l:"+a[2].length);
+	}
+
+}
