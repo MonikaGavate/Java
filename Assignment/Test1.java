5e48bd8c09590a97e5488b23ac77db9f5d31f7d0
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-03 21:42:04 +0530

    Add files via upload

---
+++ Assignment/Test1.java
@@ -0,0 +1,19 @@
+package monika;
+
+class Test1 {
+public static void main(String[] args)
+{
+int arr[] = { 11, 22, 33 };
+for (int i = 0; i < arr.length; i++)
+System.out.print(arr[i] + " ");
+System.out.println();
+//int arr1[] = new int[3];
+  // arr1[0]=11;
+   //arr1[1]=22;
+   //arr1[2]=33;
+int arr1[] = {11,22,33};
+for (int i = 0; i < arr1.length; i++)
+System.out.print(arr1[i] + " ");
+}
+
+}
