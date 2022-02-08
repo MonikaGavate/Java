c683c68b85dfc6ef1458e8409895b71cf92567bc
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-08 19:48:22 +0530

    Add files via upload

---
+++ practical/Collections/stack.java
@@ -0,0 +1,21 @@
+package monika;
+
+import java.util.*;
+
+public class stack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Stack<String>stack=new Stack<String>();
+stack.push("Ayush");
+stack.push("Garvit");
+stack.push("Amit");
+stack.push("Ashish");
+stack.push("Garima");
+stack.pop();
+Iterator<String> itr=stack.iterator();
+while(itr.hasNext()) {
+System.out.println(itr.next());
+	}
+	}
+}
