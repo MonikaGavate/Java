c118661be9befff986a77c919c76294f616a7631
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-08 23:15:54 +0530

    Add files via upload

---
+++ Assignment/CollectionsAssi/ArrayListUpdateAssi.java
@@ -0,0 +1,21 @@
+package monika;
+import java.util.*;
+public class ArrayListUpdateAssi {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		ArrayList<String>list=new ArrayList<String>();//Creating arraylist
+		list.add("Red");//Adding object in arrayList
+		list.add("Green");
+		list.add("Yellow");
+		list.add("Orange");
+		list.add("Purple");
+		list.add("blue");
+		System.out.println(list);
+		list.set(3, "white");
+		list.set(1,"Black");
+		System.out.println(list);
+		}
+	}
+	
+
