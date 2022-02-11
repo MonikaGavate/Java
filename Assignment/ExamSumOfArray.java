309aca7c763e31be86a57d79b466eaeec8e18ab7
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-11 16:51:56 +0530

    Add files via upload

---
+++ Assignment/ExamSumOfArray.java
@@ -0,0 +1,16 @@
+package monika;
+
+public class ExamSumOfArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int sum=0;
+		int a[]= {5,2,3,4,5};
+      for(int i=0;i<a.length;i++)
+      {
+    	  sum=sum+a[i];
+      }
+      System.out.println("Array="+sum);
+	}
+
+}
