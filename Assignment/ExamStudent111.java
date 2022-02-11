0b1a830b1a99228b17d4280b28d4a0a5b8bd7324
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-11 17:12:44 +0530

    Add files via upload

---
+++ Assignment/ExamStudent111.java
@@ -0,0 +1,13 @@
+package monika;
+
+public class ExamStudent111 {
+       int id;
+       String name;
+      public static void main(String[] args) {
+  		// TODO Auto-generated method stub
+    	  ExamStudent111 s=new ExamStudent111();
+    	  System.out.println(s.id);
+    	  System.out.println(s.name);
+	}
+
+}
