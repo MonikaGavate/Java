702363a0e06c096538ceeb65293a849468a9578d
MonikaGavate <95684943+MonikaGavate@users.noreply.github.com>
2022-02-14 22:16:57 +0530

    Add files via upload

---
+++ practical/MyName/src/main/java/com/monika/MyName/App.java
@@ -0,0 +1,13 @@
+package com.monika.MyName;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi, My Name Is Monika" );
+    }
+}
